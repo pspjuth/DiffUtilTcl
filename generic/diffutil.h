87bf57b31d6f57384decb5bd367a95ea880feade
peter <peter>
2007-09-30 07:27:35 +0000

    Added new commands.

--- generic/diffutil.h
+++ generic/diffutil.h
@@ -1,6 +1,21 @@
+/***********************************************************************
+ * $Revision: 1.2 $
+ ***********************************************************************/
 
-extern int DiffFilesObjCmd(ClientData dummy,
-                           Tcl_Interp *interp,
-                           int objc,
-                           Tcl_Obj *CONST objv[]);
+extern int
+DiffFilesObjCmd(ClientData dummy,
+                Tcl_Interp *interp,
+                int objc,
+                Tcl_Obj *CONST objv[]);
 
+extern int
+DiffStringsObjCmd(ClientData dummy,
+                  Tcl_Interp *interp,
+                  int objc,
+                  Tcl_Obj *CONST objv[]);
+
+extern int
+DiffStrings2ObjCmd(ClientData dummy,
+                   Tcl_Interp *interp,
+                   int objc,
+                   Tcl_Obj *CONST objv[]);
