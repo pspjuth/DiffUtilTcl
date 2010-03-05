a9f389660e4bb15cab4ab8bf8e34cca9127890ab
Peter Spjuth <peter.spjuth@gmail.com>
2010-03-05 21:51:52 +0100

    Moved files comparison specific functions to new file. Adjusted declarations as needed for the move.

--- generic/diffutil.h
+++ generic/diffutil.h
@@ -2,6 +2,98 @@
  * $Revision: 1.2 $
  ***********************************************************************/
 
+//#define CANDIDATE_DEBUG
+//#define CANDIDATE_STATS
+
+/* A type to hold hashing values */
+typedef unsigned long Hash_T;
+
+/* A type to hold line numbers */
+typedef unsigned long Line_T;
+
+/* Hold all options for diffing in a common struct */
+#define STATIC_ALIGN 10
+typedef struct {
+    /* Ignore flags */
+    int ignore;
+    /* Let empty lines be considered different in the LCS algorithm. */
+    int noempty;
+    /* Show full words in changes */
+    int wordparse;
+    /* Range */
+    Line_T rFrom1, rTo1, rFrom2, rTo2;
+    /* Alignment */
+    int alignLength;
+    Line_T *align;
+    Line_T staticAlign[STATIC_ALIGN];
+} DiffOptions_T;
+
+/* Flags in DiffOptions_T's ignore field */
+
+#define IGNORE_ALL_SPACE 1
+#define IGNORE_SPACE_CHANGE 2
+#define IGNORE_CASE 4
+#define IGNORE_NUMBERS 8
+
+/*
+ * A type to implement the V vector in the LCS algorithm.
+ * For each line (element) in "file 2", this struct holds
+ * the line number (serial) and the hash values for the line.
+ *
+ * The hashes are split in hash and realhash. The former is calculated
+ * taking account ignore options and is used for matching lines.
+ * The latter hashes the full line, to give the algorithm the
+ * possibility to prefer exact matches.
+ */
+typedef struct {
+    Line_T serial;
+    Hash_T hash;
+    Hash_T realhash;
+} V_T;
+
+/*
+ * A type to implement the E vector in the LCS algorithm.
+ * The E vector mirrors the sorted V vector and holds equivalence
+ * classes of lines in "file 2".
+ */
+typedef struct {
+    Line_T serial;
+    int last;        /* True on the last element of each class */
+    int count;       /* On the first in each class, keeps the number
+		      * of lines in the class. Otherwise zero. */
+    Hash_T realhash; /* Keep the realhash for reference */
+} E_T;
+
+/*
+ * A type to implement the P vector in the LCS algorithm.
+ *
+ * This reflects each line in "file 1" and points to the equivalent
+ * class in the E vector.
+ * A zero means there is no matching line in "file 2".
+ */
+typedef struct {
+    Line_T Eindex;   /* First element in equivalent class in E vector */
+    Hash_T realhash; /* Keep the realhash for reference */
+} P_T;
+
+
+extern E_T *     BuildEVector(V_T *V, Line_T n);
+extern int       CompareObjects(Tcl_Obj *obj1Ptr, Tcl_Obj *obj2Ptr,
+			DiffOptions_T *optsPtr);
+extern int       CompareV(const void *a1, const void *a2);
+extern void      Hash(Tcl_Obj *objPtr, DiffOptions_T *optsPtr, Hash_T *result,
+			Hash_T *real);
+extern Line_T *  LcsCore(Tcl_Interp *interp, Line_T m, Line_T n, P_T *P,
+			E_T *E, DiffOptions_T *optsPtr);
+extern Tcl_Obj * NewChunk(Tcl_Interp *interp, DiffOptions_T *optsPtr,
+			Line_T start1, Line_T n1, Line_T start2, Line_T n2);
+extern void      NormaliseOpts(DiffOptions_T *optsPtr);
+extern int       SetOptsRange(Tcl_Interp *interp, Tcl_Obj *rangePtr, int first,
+			DiffOptions_T *optsPtr);
+extern int       SetOptsAlign(Tcl_Interp *interp, Tcl_Obj *alignPtr, int first,
+			DiffOptions_T *optsPtr);
+
+
 extern int
 DiffFilesObjCmd(ClientData dummy,
                 Tcl_Interp *interp,
