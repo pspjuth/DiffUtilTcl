7c9a598ab5bedde891d51f5520b3d260c49c2b52
Peter Spjuth <peter.spjuth@gmail.com>
2010-06-12 00:54:52 +0200

    Bumped revision to 0.2.1

--- tcl/diffutil.tcl
+++ tcl/diffutil.tcl
@@ -11,7 +11,7 @@
 # $Revision: 1.12 $
 #----------------------------------------------------------------------
 
-package provide DiffUtil 0.2
+package provide DiffUtil 0.2.1
 
 namespace eval DiffUtil {
     namespace export diffFiles diffStrings
