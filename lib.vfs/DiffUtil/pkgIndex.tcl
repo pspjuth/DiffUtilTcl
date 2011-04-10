284a826fdaf86013d9eff27d54b08568e2db3b0b
Peter Spjuth <peter.spjuth@gmail.com>
2011-04-10 02:19:14 +0200

    Repaired vfs target

--- lib.vfs/DiffUtil/pkgIndex.tcl
+++ lib.vfs/DiffUtil/pkgIndex.tcl
@@ -1,3 +1,3 @@
-package ifneeded DiffUtil 0.3 \
+package ifneeded DiffUtil 0.3.2 \
     "[list source [file join $dir loadLib.tcl]]; \
-    [list ::DiffUtil::_load $dir libDiffUtil0.3.so 0.3]"
+    [list ::DiffUtil::_load $dir libDiffUtil0.3.2.so 0.3.2]"
--- tools/loadLib.tcl
+++ tools/loadLib.tcl
@@ -1,14 +1,28 @@
 # Provide a load function for a Starkit version of the package.
 
 namespace eval ::DiffUtil {
-    proc _load {dir libfile version} {
-        if {$::tcl_platform(platform) == "windows"} {
-            set root [file rootname $libfile]
-            set root [string map {. {}} $root]
-            set libfile $root[info sharedlibext]
+    # return a platform designator, including both OS and machine
+    proc _platform {} {
+        global tcl_platform
+        set plat [lindex $tcl_platform(os) 0]
+        set mach $tcl_platform(machine)
+        switch -glob -- $mach {
+            sun4* { set mach sparc }
+            intel -
+            i*86* { set mach x86 }
+            "Power Macintosh" { set mach ppc }
         }
-        if {[catch {uplevel \#0 [list load [file join $dir $libfile]]}]} {
-            uplevel \#0 [list source [file join $dir diffutil.tcl]]
+        return "$plat-$mach"
+    }
+    proc _load {dir libfile version} {
+        set root [file rootname $libfile]
+        if {$::tcl_platform(platform) == "windows"} {
+            set root [string map {. {}} $root]
+        }
+        set libfile $root[info sharedlibextension]
+        set fulllibfile [file join $dir [_platform] $libfile]
+        if {[catch {uplevel \#0 [list load $fulllibfile]}]} {
+            uplevel \#0 [list source [file join $dir tcl diffutil.tcl]]
         }
     }
 }
