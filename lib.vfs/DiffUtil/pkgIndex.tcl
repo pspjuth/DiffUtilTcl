1785be7e77a18de2aab5cae18910b34f2812ac9e
Peter Spjuth <peter.spjuth@gmail.com>
2016-08-15 00:42:51 +0200

    Bumped revision to 0.3.10

--- lib.vfs/DiffUtil/pkgIndex.tcl
+++ lib.vfs/DiffUtil/pkgIndex.tcl
@@ -1,3 +1,3 @@
-package ifneeded DiffUtil 0.3.9 \
+package ifneeded DiffUtil 0.3.10 \
     "[list source [file join $dir loadLib.tcl]]; \
-    [list ::DiffUtil::_load $dir libDiffUtil0.3.9.so 0.3.9]"
+    [list ::DiffUtil::_load $dir libDiffUtil0.3.10.so 0.3.10]"
