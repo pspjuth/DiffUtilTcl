0970ea04d2245dd4ab69201b8fe3633bf9227ef8
Peter Spjuth <peter.spjuth@gmail.com>
2017-12-02 02:31:04 +0100

    Bumped revision to 0.3.11

--- lib.vfs/DiffUtil/pkgIndex.tcl
+++ lib.vfs/DiffUtil/pkgIndex.tcl
@@ -1,3 +1,3 @@
-package ifneeded DiffUtil 0.3.10 \
+package ifneeded DiffUtil 0.3.11 \
     "[list source [file join $dir loadLib.tcl]]; \
-    [list ::DiffUtil::_load $dir libDiffUtil0.3.10.so 0.3.10]"
+    [list ::DiffUtil::_load $dir libDiffUtil0.3.11.so 0.3.11]"
--- tcl/diffutil.tcl
+++ tcl/diffutil.tcl
@@ -11,7 +11,7 @@
 # $Revision: 1.12 $
 #----------------------------------------------------------------------
 
-package provide DiffUtil 0.3.10
+package provide DiffUtil 0.3.11
 
 namespace eval DiffUtil {
     namespace export diffFiles diffStrings
