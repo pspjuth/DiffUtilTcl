a3c35f92d8084d18f480a25487c9c64c6dfb3200
Peter Spjuth <peter.spjuth@gmail.com>
2017-12-02 02:04:56 +0100

    Stored profiling notes.

--- tests/big.tcl
+++ tests/big.tcl
@@ -2,9 +2,18 @@ package require DiffUtil
 puts "P:[info procs DiffUtil::*]"
 #DiffUtil::diffFiles -b tests/bigfilex1 tests/bigfilex2
 
-set s1 {AjkHFAkjaaslkJADADlkhaDkhALDKJHDA}
-set s2 {AjkhfAkqqAslkJAdaDlsxaDkhAftKJHDA}
+#set s1 {AjkHFAkjaaslkJADADlkhaDkhALDKJHDA}
+#set s2 {AjkhfAkqqAslkJAdaDlsxaDkhAftKJHDA}
 
-set n 10000
-puts [time {DiffUtil::diffStrings $s1 $s2} $n]
+#set n 10000
+#puts [time {DiffUtil::diffStrings $s1 $s2} $n]
 #puts [time {DiffUtil::diffStrings2 $s1 $s2} $n]
+
+set f1 tests/creole1.xml
+set f2 tests/creole2.xml
+
+set n 1
+puts [time {DiffUtil::diffFiles -pivot 5 $f1 $f2} $n]
+puts [time {DiffUtil::diffFiles -pivot 10 $f1 $f2} $n]
+puts [time {DiffUtil::diffFiles -pivot 5 $f1 $f2} $n]
+puts [time {DiffUtil::diffFiles -pivot 10 $f1 $f2} $n]
