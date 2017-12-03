5783ba327fe73d0776e727b76cdb90e57c3d3abb
Peter Spjuth <peter.spjuth@gmail.com>
2017-12-03 22:16:30 +0100

    Update VFS to 0.40

--- lib.vfs/DiffUtil/pkgIndex.tcl
+++ lib.vfs/DiffUtil/pkgIndex.tcl
@@ -1,3 +1,3 @@
-package ifneeded DiffUtil 0.3.11 \
+package ifneeded DiffUtil 0.4.0 \
     "[list source [file join $dir loadLib.tcl]]; \
-    [list ::DiffUtil::_load $dir libDiffUtil0.3.11.so 0.3.11]"
+    [list ::DiffUtil::_load $dir libDiffUtil0.4.0.so 0.4.0]"
