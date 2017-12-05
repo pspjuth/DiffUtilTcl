a6499eb2b0a42695d152f41f5a0ebf2ddf215279
Peter Spjuth <peter.spjuth@gmail.com>
2017-12-05 01:14:22 +0100

    Updated VFS build

--- makevfs.sh
+++ makevfs.sh
@@ -1,9 +1,7 @@
 # Script to build a multi-platform vfs
 
 # Cross-compiling needs a cross-compiled tcl for linking
-# _USE_32BIT_TIME_T is needed to get Tcl_StatBuf to work, for some reason
-# If we switch to use Tcl8.6 functions the dependency on Tcl_StatBuf disappears
-CFLAGS=-D_USE_32BIT_TIME_T ./configure --host=i586-mingw32msvc --target=i586-mingw32msvc --with-tcl=/home/peter/tcl/win/tcl/win
+./configure --host=i586-mingw32msvc --target=i586-mingw32msvc --with-tcl=/home/peter/tcl/win/tcl/win
 make clean
 make
 make vfswin
@@ -14,7 +12,7 @@ make clean
 make
 make vfs32
 
-./configure --enable-64bit
+./configure --enable-64bit --with-tcl=/home/peter/tcl/v86/tcl/unix
 make clean
 make
 make vfs64
